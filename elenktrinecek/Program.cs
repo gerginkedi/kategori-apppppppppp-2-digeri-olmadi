2c64d53bbf82ba358baf3d182d3350b3ae62b740
gato roboto <omerinyerix@gmail.com>
2025-06-30 17:13:19 +0300

    Add project files.

---
+++ elenktrinecek/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Net.Sockets;
+
+namespace elenktrinecek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = true;
+            bool isActive1 = true;
+            string categoryForLooking = string.Empty;
+            string itemOrExit = string.Empty;
+
+            Suffixer suffixer = new Suffixer();
+
+            Dictionary<string, List<string>> menu = new Dictionary<string, List<string>>();
+
+            List<string> categoryNamesComeFromSuffixer = new List<string>();
+
+            foreach (string item in suffixer.categoryItemSuffixer())
+            {
+                categoryNamesComeFromSuffixer.Add(item);
+            }
+
+            foreach (string item in categoryNamesComeFromSuffixer)
+            {
+                menu.Add(item, new List<string>());
+            }
+
+            while (isActive)
+            {
+
+                Console.WriteLine(" Hangi kategoriye item ekleme islemi yapilsin");
+                Console.WriteLine("(cikmak icin 0)");
+                Console.WriteLine("===================================");
+                foreach (string kategoriler in categoryNamesComeFromSuffixer)
+                {
+                    Console.WriteLine(kategoriler.ToUpper());
+                }
+
+
+
+                //foreach (string item in categoryNamesComeFromSuffixer)
+                //{
+                //    categoryForLooking = Console.ReadLine();
+
+                //    if (item == categoryForLooking)
+                //    {
+                //        while (isActive1)
+                //        {
+                //            Console.Write("eklenecek item adi giriniz: ");
+                //            Console.Write("cikmak iicn 0");
+                //            Console.WriteLine("///////////////");
+
+                //            itemOrExit = Console.ReadLine();
+
+                //            if (itemOrExit == "0")
+                //            {
+                //                isActive1 = false;
+                //            }
+
+                //            menu[categoryForLooking].Add(itemOrExit);
+                //        }
+                //    }
+
+                //    else
+                //    {
+                //        Console.WriteLine("boyle bir kategori yok");
+                //    }
+                //}
+
+                Console.WriteLine("//////////////////////////////////////////////////////////////////");
+                Console.WriteLine(categoryForLooking.ToUpper());
+                foreach (string sil in menu[categoryForLooking])
+                {
+                    Console.WriteLine(sil.ToLower());
+                }
+
+            }
+        }
+    }
+}
---
+++ elenktrinecek/Suffixer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elenktrinecek
+{
+    internal class Suffixer
+    {
+
+        public List<string> categoryItemSuffixer()
+        {
+            List<string> listToReturn = new List<string>();
+            bool isActive = true;
+            string categoryNameToAdd = string.Empty;
+            string input = string.Empty;
+
+            while (isActive)
+            {
+
+                Console.Write("eklenecek kategorinin adini giriniz: ");
+                categoryNameToAdd = Console.ReadLine();
+                listToReturn.Add(categoryNameToAdd);
+                Console.WriteLine("cikmak icin 0, devam etmek icin 1 yaziniz: ");
+                input = Console.ReadLine();
+
+                if (input == "0")
+                {
+                    isActive = false;
+                }
+
+            }
+
+            return listToReturn;
+        }
+    }
+}
